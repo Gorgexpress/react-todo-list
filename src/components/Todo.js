e96a517ad749f78af85aca53dd83ba2ba893f948
Michael <gorgexpress@gmail.com>
2017-02-14 22:49:19 -0500

    init commit

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+const Todo = ({text, id}) => (
+  <li>{text}</li>
+)
+
+Todo.propTypes = {
+  text: PropTypes.string.isRequired
+}
+
+export default Todo
